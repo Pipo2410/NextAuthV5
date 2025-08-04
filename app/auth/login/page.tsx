70a8eafa4967fc86e6574a0318b55d454621e7ed
Pipo2410 <Pipo2410@gmail.com>
2025-08-04 21:33:03 +0100

    fix vercel suspense error, add skeleton

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { LoginForm } from '@/components/auth/login-form'
-
+import { Skeleton } from '@/components/ui/skeleton'
 const LoginPage = () => {
-  return <LoginForm />
+  return (
+    <Suspense
+      fallback={<Skeleton className='h-[464px] w-[400px] rounded-xl' />}
+    >
+      <LoginForm />
+    </Suspense>
+  )
 }
 
 export default LoginPage
