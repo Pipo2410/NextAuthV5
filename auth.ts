4d3e3df829799442867b3609a00772c6a25decf2
Pipo2410 <Pipo2410@gmail.com>
2025-08-06 22:09:18 +0100

    Send verification email - callbacks(signIn), verificationToken prisma model, generate token, implement Resend(email client)

--- auth.ts
+++ auth.ts
@@ -24,6 +24,19 @@ export const {
     },
   },
   callbacks: {
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      if (account?.provider !== 'credentials') return true
+
+      const existingUser = user.id ? await getUserById(user.id) : null
+
+      // Prevent sign in without email verification
+      if (!existingUser?.emailVerified) return false
+
+      // Add 2FA check
+
+      return true
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub
